8565ba316361bd544eb0d8c0dde8ab503ae7f735
budhip <ryo.bunraku@yahoo.com>
2017-07-13 21:26:46 +0700

    validasi email unique saat add data sudah jalan, tapi pada saat edit belum berhasil

--- models/student.js
+++ models/student.js
@@ -3,7 +3,28 @@ module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      isEmail: true,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isUnique: function (value, next) {
+          var self = this;
+          Student.find({where: {email: value}})
+          .then(function (student) {
+            // tolak jika ada yang memasukkan email yang sama
+            if (student && self.id !== student.id) {
+              return next('Email udah ada yang pake om!');
+            }
+              return next();
+            })
+            .catch(function (err) {
+              return next(err);
+              });
+            }
+          }
+        },
     jurusan: DataTypes.STRING
   }, {
     classMethods: {
--- routers/student.js
+++ routers/student.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var Sequelize = require('sequelize')
 
 const Student = require('../models');
 
@@ -7,7 +8,10 @@ const Student = require('../models');
 router.get('/', function (req, res) {
   Student.Student.findAll()
   .then(data => {
-    res.render('student', {dataStudent: data});
+    res.render('student', {dataStudent: data, msg: ""});
+  })
+  .catch(Sequelize.ValidationError, function (err) {
+    res.render('student', {dataStudent:data, err: err})
   })
 })
 
@@ -21,14 +25,53 @@ router.post('/', function(req, res) {
   .then(function(){
     res.redirect('/student')
   })
+  .catch(Sequelize.ValidationError, function (err) {
+    // console.log(`ini hasil ${err.errors[0].message}`);
+    // return res.status(422).send(err.errors);
+    Student.Student.findAll()
+    .then(data => {
+      res.render('student', {dataStudent: data, msg: err.errors[0].message});
+    })
+  })
 });
 
 router.get('/edit/:id', function(req, res) {
-  
+  Student.Student.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    // console.log(data.id);
+    res.render('student-edit', {data:data})
+  })
 })
 
 router.post('/update/:id', function(req, res) {
+  Student.Student.update({
+    first_name : req.body.first_name,
+    last_name : req.body.last_name,
+    email : req.body.email,
+    jurusan : req.body.jurusan
+  },{
+    where: {
+      id : req.params.id
+    }
+  })
+  .then(function(){
+    res.redirect('/student')
+  })
+})
 
+router.get('/delete/:id', function(req, res) {
+  Student.Student.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    res.redirect('/student');
+  })
 })
 
 module.exports = router
